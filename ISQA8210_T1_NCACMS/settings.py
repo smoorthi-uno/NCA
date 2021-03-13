f21cc718b8230a088f19949a0c86ca137bf2ff17
Swetha Moorthi <smoorthi@unomaha.edu>
2021-03-12 19:09:24 -0600

    Added migrations folder
    Added migrations folder

--- ISQA8210_T1_NCACMS/settings.py
+++ ISQA8210_T1_NCACMS/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'nca',
     'crispy_forms',
 ]
@@ -86,8 +87,10 @@ WSGI_APPLICATION = 'ISQA8210_T1_NCACMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ncaDb',
+        'USER': 'instructor',
+        'PASSWORD': 'instructor1a'
     }
 }
 
